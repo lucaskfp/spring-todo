11608afa482a201b36faa60cfc260c9228b7adcc
Lucas Kennedy <lukakfp@gmail.com>
2022-04-08 11:00:42 -0300

    first commit

---
+++ src/main/java/br/com/lucaskfp/springtodo/SpringtodoApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucaskfp.springtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtodoApplication.class, args);
+	}
+
+}
