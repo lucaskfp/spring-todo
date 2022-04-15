5093f491a75195b30802bb98933c16e6f696a40c
Lucas Kennedy <lukakfp@gmail.com>
2022-04-15 16:03:37 -0300

    refactor

--- src/main/java/br/com/lucaskfp/springtodo/user/UserRepository.java
+++ src/main/java/br/com/lucaskfp/springtodo/user/UserRepository.java
@@ -1,13 +1,11 @@
 package br.com.lucaskfp.springtodo.user;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
-    public Optional<UserEntity> findByEmail(String email);
+    public UserEntity findByEmail(String email);
 
 }
--- src/main/java/br/com/lucaskfp/springtodo/user/validations/UniqueEmailValidator.java
+++ src/main/java/br/com/lucaskfp/springtodo/user/validations/UniqueEmailValidator.java
@@ -3,18 +3,19 @@ package br.com.lucaskfp.springtodo.user.validations;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import br.com.lucaskfp.springtodo.user.UserEntity;
 import br.com.lucaskfp.springtodo.user.UserRepository;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
 
-    private final UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        UserEntity inDB = userRepository.findByEmail(value).orElse(null);
+        UserEntity inDB = userRepository.findByEmail(value);
 
         if (inDB == null) {
             return true;
