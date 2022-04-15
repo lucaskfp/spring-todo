2ad11edc96b0be110bd46827be85d697792f8a70
Lucas Kennedy <lukakfp@gmail.com>
2022-04-15 02:01:27 -0300

    fix: correção do texto da resposta

--- src/main/java/br/com/lucaskfp/springtodo/common/errors/EmailNotAvailableException.java
+++ src/main/java/br/com/lucaskfp/springtodo/common/errors/EmailNotAvailableException.java
@@ -1,8 +1,13 @@
 package br.com.lucaskfp.springtodo.common.errors;
 
-public class EmailNotAvailableException extends Exception {
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class EmailNotAvailableException extends DataIntegrityViolationException {
 
     public EmailNotAvailableException() {
-        super("Email indisponível");
+        super("Este email já está em uso. Tente outro.");
     }
 }
