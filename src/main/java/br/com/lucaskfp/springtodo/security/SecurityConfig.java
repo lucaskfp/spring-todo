05b9394ec36364ac8438e2b5061153148ddfc147
Lucas Kennedy <lukakfp@gmail.com>
2022-04-17 17:16:55 -0300

    feat: configuração para usar token jwt

--- src/main/java/br/com/lucaskfp/springtodo/security/SecurityConfig.java
+++ src/main/java/br/com/lucaskfp/springtodo/security/SecurityConfig.java
@@ -1,35 +1,33 @@
 package br.com.lucaskfp.springtodo.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    CustomUserDetailsService customUserDetailsService;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-    }
+    TokenFilter tokenFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
+        http.csrf().disable();
+
+        http
                 .authorizeRequests()
-                .antMatchers("/users/register").permitAll()
+                .antMatchers("/users/register", "/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
 
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
 }
